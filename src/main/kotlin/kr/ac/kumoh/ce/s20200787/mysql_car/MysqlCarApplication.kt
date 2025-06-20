f634b3c4cc19ac2be5823589c42ec51b6d4154ae
microapk <microapk@naver.com>
2025-06-20 16:50:59 +0900

    데이터베이스 연결 성공

---
+++ src/main/kotlin/kr/ac/kumoh/ce/s20200787/mysql_car/MysqlCarApplication.kt
@@ -0,0 +1,11 @@
+package kr.ac.kumoh.ce.s20200787.mysql_car
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MysqlCarApplication
+
+fun main(args: Array<String>) {
+	runApplication<MysqlCarApplication>(*args)
+}
---
+++ src/test/kotlin/kr/ac/kumoh/ce/s20200787/mysql_car/MysqlCarApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.ce.s20200787.mysql_car
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MysqlCarApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
